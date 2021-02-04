c9b21545daa5c84a0041476f90bc027cc944a377
Lbniese <lbniese@gmail.com>
2021-02-04 15:43:51 +0100

    Assignment 1 and 2

---
+++ 2/0._Misc/1._Intro_Exercises/2.Strings.js
@@ -0,0 +1,58 @@
+// --------------------------------------
+// Exercise 3 - Add numbers from string to float
+
+const numberOne = "1.10";
+const numberTwo = "2.30";
+
+// add those two numbers and show the result
+// you cannot touch line 1 neither line 2
+
+const total1 = parseFloat(numberOne) + parseFloat(numberTwo);
+
+console.log(total1);
+
+// --------------------------------------
+
+// --------------------------------------
+// Exercise 4 - Add the numbers and the total with 2 decimals
+
+const anotherNumberOne = "1.10";
+const anotherNumberTwo = "2.30";
+
+const total2 = parseFloat(anotherNumberOne) + parseFloat(anotherNumberTwo);
+
+console.log((total2 + total2).toFixed(2));
+
+// --------------------------------------
+// Exercise 5 - Decimals and average
+
+const one = 10;
+const two = 45;
+const three = 98;
+
+// Show in the console the avg. with 5 decimals
+
+
+console.log(((one + two + three) / 3).toFixed(5));
+
+
+// --------------------------------------
+// Exercise 6 - Get the character by index
+
+const letters = "abc";
+// Get me the character "c"
+
+console.log(letters.charAt(2));
+
+// --------------------------------------
+// Exercise 7 - Replace
+
+const fact = "You are learning javascript!";
+
+// capitalize the J in Javascript
+
+console.log(fact.replace("j","J"));
+
+// --------------------------------------
+
+
