50b84c510d7c76c63fb29c19c57852ee5190441d
Lbniese <lbniese@gmail.com>
2021-02-18 09:51:22 +0100

    2._Rest_API_Design - GET

---
+++ 2/1._First_Server/app.js
@@ -0,0 +1,39 @@
+//import express
+const express = require("express");
+// instantiate express
+const app = express();
+
+// hard core persons
+const persons = [
+  {
+    id: 1,
+    name: "Lukas",
+  },
+  {
+    id: 2,
+    name: "Mikkel",
+  },
+  {
+    id: 3,
+    name: "Peter",
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send({
+    message:
+      "Visit /persons for all persons or /persons/id for specific perons",
+  });
+});
+
+app.get("/persons", (req, res) => {
+  res.send(persons);
+});
+
+app.get("/persons/:id", (req, res) => {
+  let personsList = persons.filter((person) => person.id == req.params.id);
+  res.send(personsList);
+});
+
+//listen to port 5000
+app.listen(5000);
