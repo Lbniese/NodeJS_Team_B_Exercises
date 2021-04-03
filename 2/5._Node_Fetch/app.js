021f7c7e2cdaa1623c3743cdb4220beb8c86f0a5
Lbniese <lbniese@gmail.com>
2021-04-03 12:28:37 +0200

    added the serve functionality
    *website will redirect from / to /proxy
    *code can both use an actual proxy or not now

--- 2/5._Node_Fetch/app.js
+++ 2/5._Node_Fetch/app.js
@@ -6,20 +6,35 @@ const app = express();
 const fetch = require('node-fetch');
 // software version number
 const version = "1.0.0";
+// define which port to listen on
+const port = 5000;
+
+app.get('/', (req, res) => {
+  res.redirect('/proxy');
+});
 
 
+// fetch using NO proxy
+app.get('/proxy', (req, res) => {
+  (async () => {
+      const response = await fetch('https://www.google.com');
+      const body = await response.text();
+      //console.log(body);
+      res.send(body);
+  })();
+});
+
+// fetch using an actual proxy
+/*
 app.get('/proxy', (req, res) => {
     (async () => {
         const response = await fetch('https://app.scrapingbee.com/api/v1?api_key=0G7R0MGKCQ72SEZLKADQYJW6HULSCI2XP1WRKNGR28LE22FMOZ19WK21AEEXO5Y3WI1BIF4ETTBWSQ6Y&custom_google=True&url=https://www.google.com');
         const body = await response.text();
-        console.log(body);
+        //console.log(body);
+        res.send(body);
     })();
 });
-
-
-
-// define which port to listen on
-const port = 5000;
+*/
 
 // listen to a port and start web server
 app.listen(port, (error) => {
